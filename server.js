48ee4d5c56cdf0d433f0ec5465863131b5c74ab6
Ernst Pierre <emmp318@gmail.com>
2020-09-08 15:34:49 -0500

    updated server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
@@ -15,7 +15,8 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 
 }, () => 
 console.log("Connected to DB")
@@ -26,5 +27,5 @@ app.use(require("./routes/api-routes"));
 app.use(require("./routes/html-routes"));
 
 app.listen(PORT, () => {
-  console.log(`🌎 App running on port ${PORT}!`);
+  console.log(`App running on port ${PORT}!`);
 });
